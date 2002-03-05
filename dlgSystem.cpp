0dd1429491f651a2b68dd332e76a53e3246f088f
Patrick Charbonnier <pch@freeshell.org>
2002-03-05 15:22:46 +0000

    no message
    svn path=/trunk/kdenetwork/kget/; revision=141158

--- dlgSystem.cpp
+++ dlgSystem.cpp
@@ -2,10 +2,10 @@
                                 dlgSystem.cpp
                              -------------------
     Revision 				: $Id$
-    begin						: Tue Jan 29 2002
+    begin				: Tue Jan 29 2002
     copyright				: (C) 2002 by Patrick Charbonnier
-									: Based On Caitoo v.0.7.3 (c) 1998 - 2000, Matej Koss
-    email						: pch@freeshell.og
+					: Based On Caitoo v.0.7.3 (c) 1998 - 2000, Matej Koss
+    email				: pch@freeshell.org
  ***************************************************************************/
 
 /***************************************************************************
