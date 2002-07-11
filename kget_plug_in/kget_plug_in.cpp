d7dc25cff5dc1550a9c88a1bce3a9ade49638f51
Patrick Charbonnier <pch@freeshell.org>
2002-07-11 22:24:04 +0000

    added konqueror plugin
    svn path=/trunk/kdenetwork/kget/; revision=166127

---
+++ kget_plug_in/kget_plug_in.cpp
@@ -0,0 +1,86 @@
+/***************************************************************************
+                          kget_plug_in.cpp  -  description
+                             -------------------
+    begin                : Wed Jul  3 22:09:28 CEST 2002
+    copyright            : (C) 2002 by Patrick
+    email                : pch@valleeurpe.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <khtml_part.h> // this plugin applies to a khtml part
+#include <kdebug.h>
+#include "kget_plug_in.h"
+#include <kaction.h>
+#include <kinstance.h>
+#include <kiconloader.h>
+#include <qmessagebox.h>
+#include <klocale.h>
+#include <krun.h>
+
+KGet_plug_in::KGet_plug_in( QObject* parent, const char* name )
+        : Plugin( parent, name )
+{
+    m_paToggleDropTarget=new KAction(i18n("Show Kget drop target"),
+                                     "kget_plug_in", 0,
+                                     this, SLOT(slotShowDrop()),
+                                     actionCollection(), "show_drop" );
+    p_dcopServer= new DCOPClient();
+
+    p_dcopServer->attach ();
+
+
+}
+
+KGet_plug_in::~KGet_plug_in()
+{
+    p_dcopServer->detach();
+    delete p_dcopServer;
+}
+
+
+void KGet_plug_in::slotShowDrop()
+{
+    if (!p_dcopServer->isApplicationRegistered ("kget"))
+        KRun::runCommand("kget --showDropTarget");
+    else
+    {
+      QByteArray data;
+      QDataStream arg(data, IO_WriteOnly);
+      arg << QCString("drop_target");
+      p_dcopServer->send("kget", "kget-mainwindow#1", (QCString) "activateAction(QCString)", data);
+    }
+}
+
+KPluginFactory::KPluginFactory( QObject* parent, const char* name )
+        : KLibFactory( parent, name )
+{
+    s_instance = new KInstance("KPluginFactory");
+}
+
+QObject* KPluginFactory::createObject( QObject* parent, const char* name, const char*, const QStringList & )
+{
+    QObject *obj = new KGet_plug_in( parent, name );
+    return obj;
+}
+KPluginFactory::~KPluginFactory()
+{ delete s_instance; }
+
+
+extern "C"
+{
+    void* init_libkget_plug_in()
+    {
+        return new KPluginFactory;
+    }
+
+}
+
+KInstance* KPluginFactory::s_instance = 0L;
---
+++ kget_plug_in/kget_plug_in.h
@@ -0,0 +1,56 @@
+/***************************************************************************
+                          kget_plug_in.h  -  description
+                             -------------------
+    begin                : Wed Jul  3 22:09:28 CEST 2002
+    copyright            : (C) 2002 by Patrick
+    email                : pch@valleeurpe.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef __plugin_kget_plug_in_h
+#define __plugin_kget_plug_in_h
+
+#include <kparts/plugin.h>
+#include <klibloader.h>
+#include <dcopclient.h>
+
+class KURL;
+class KInstance;
+
+class KGet_plug_in : public KParts::Plugin
+{
+    Q_OBJECT
+public:
+    KGet_plug_in( QObject* parent = 0, const char* name = 0 );
+    KAction *m_paToggleDropTarget ;
+    DCOPClient* p_dcopServer;
+    virtual ~KGet_plug_in();
+
+public slots:
+    void slotShowDrop();
+};
+
+
+class KPluginFactory : public KLibFactory
+{
+    Q_OBJECT
+public:
+    KPluginFactory( QObject *parent = 0, const char *name = 0 );
+    ~KPluginFactory() ;
+
+    virtual QObject* createObject( QObject* parent = 0, const char* pname = 0,
+                                   const char* name = "QObject",
+                                   const QStringList &args = QStringList() );
+
+private:
+    static KInstance* s_instance;
+};
+
+#endif
