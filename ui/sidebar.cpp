4cf62bb298f7314d285f3cfaea56bc7b7d04c89f
Dario Massarin <nekkar@libero.it>
2005-01-16 00:07:13 +0000

    Adding copyright
    svn path=/branches/make_kget_cool/kdenetwork/kget/; revision=378902

--- ui/sidebar.cpp
+++ ui/sidebar.cpp
@@ -1,3 +1,13 @@
+/* This file is part of the KDE project
+
+   Copyright (C) 2004 Dario Massarin <nekkar@libero.it>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; version 2
+   of the License.
+*/
+
 #include <qpainter.h>
 #include <qvbox.h>
 #include <qimage.h>
