cbbfb56e6e8b327397b667f95ec0557d9e2f6413
Manolo Valdes <nolis71cu@gmail.com>
2006-02-10 19:08:35 +0000

    mt plugin details widget added
    svn path=/branches/work/make_kget_cool/kget/; revision=508100

---
+++ transfers/multithreaded/mtdetailswidget.cpp
@@ -0,0 +1,17 @@
+/* This file is part of the KDE project
+
+   Copyright (C) 2006 Manolo Valdes <nolis71cu@gmail.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; version 2
+   of the License.
+*/
+
+#include "mtdetailswidget.h"
+
+MTDetailsWidget::MTDetailsWidget()
+{
+    Ui::MTDetails frm;
+    frm.setupUi(this);
+}
---
+++ transfers/multithreaded/mtdetailswidget.h
@@ -0,0 +1,25 @@
+/* This file is part of the KDE project
+
+   Copyright (C) 2006 Manolo Valdes <nolis71cu@gmail.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; version 2
+   of the License.
+*/
+
+#ifndef MTDETAILSWIDGET_H
+#define MTDETAILSWIDGET_H
+
+#include <QWidget>
+
+#include "mtdetailswidgetfrm.h"
+
+class MTDetailsWidget : public QWidget
+{
+    Q_OBJECT
+    public:
+        MTDetailsWidget();
+};
+
+#endif // MTDETAILSWIDGET_H
