e1097e1990c1348408d33cd7cc776ffa843904b1
Urs Wolfer <uwolfer@kde.org>
2007-03-30 19:43:11 +0000

    Add skeleton for new TransferContainer class: this class will handle every single transfer, e.g. for using metalink.
    The TransferFlags enum saves the properties of a transfer.
    
    The class does nothing at the moment.
    
    svn path=/trunk/KDE/kdenetwork/kget/; revision=648230

---
+++ core/transfercontainer.cpp
@@ -0,0 +1,23 @@
+/* This file is part of the KDE project
+
+   Copyright (C) 2007 Urs Wolfer <uwolfer @ kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; version 2
+   of the License.
+*/
+
+#include <KDebug>
+
+#include "core/transfercontainer.h"
+
+TransferContainer::TransferContainer()
+{
+}
+
+TransferContainer::~TransferContainer()
+{
+}
+
+#include "transfercontainer.moc"
---
+++ core/transfercontainer.h
@@ -0,0 +1,46 @@
+/* This file is part of the KDE project
+
+   Copyright (C) 2007 Urs Wolfer <uwolfer @ kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; version 2
+   of the License.
+*/
+
+#ifndef TRANSFERCONTAINER_H
+#define TRANSFERCONTAINER_H
+
+#include <QObject>
+
+/**
+ * @brief TransferContainer class handles the transfers
+ *
+ * This class is the base for every single transfer.
+ *
+ */
+
+class TransferContainer : public QObject
+{
+    Q_OBJECT
+
+public:
+    Q_FLAGS(TransferFlags)
+
+    enum TransferFlag {
+        /**  The transfer supports resuming and seeking  */
+        Tf_SupportsSegments    = 0x01,
+        /**  The container includes more than one file, e.g. metalink  */
+        Tf_MultipleFiles       = 0x02
+    };
+
+    Q_DECLARE_FLAGS(TransferFlags, TransferFlag)
+
+    TransferContainer();
+    ~TransferContainer();
+
+};
+
+Q_DECLARE_OPERATORS_FOR_FLAGS(TransferContainer::TransferFlags)
+
+#endif
