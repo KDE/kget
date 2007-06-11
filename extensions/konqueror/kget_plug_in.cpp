6f91c32af1ef0b62e00be1c668b3701c8f520948
Luboš Luňák <l.lunak@kde.org>
2007-06-11 15:27:33 +0000

    Pass parent to KRun::runCommand().
    svn path=/trunk/KDE/kdenetwork/kget/; revision=674007

--- extensions/konqueror/kget_plug_in.cpp
+++ extensions/konqueror/kget_plug_in.cpp
@@ -80,7 +80,8 @@ void KGet_plug_in::showPopup()
 void KGet_plug_in::slotShowDrop()
 {
     if(!QDBusConnection::sessionBus().interface()->isServiceRegistered("org.kde.kget"))
-        KRun::runCommand("kget --showDropTarget");
+        KRun::runCommand("kget --showDropTarget", parent() && parent()->inherits( "KHTMLPart" )
+            ? static_cast<KHTMLPart*>(parent())->widget() : NULL );
     else
     {
 	OrgKdeKgetInterface kgetInterface("org.kde.kget", "/KGet", QDBusConnection::sessionBus());
