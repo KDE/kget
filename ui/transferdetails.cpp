28f61d535b9ae8a603f7f8c22b4e50ea07ef657a
Urs Wolfer <uwolfer@kde.org>
2007-11-09 18:16:29 +0000

    Relicense all GPLv2 only code to GPLv2+.
    Agreed by all copyright-owners. See http://lists.kde.org/?t=119454388200001&r=1&w=2
    
    svn path=/trunk/KDE/kdenetwork/kget/; revision=734722

--- ui/transferdetails.cpp
+++ ui/transferdetails.cpp
@@ -4,9 +4,10 @@
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public
-   License as published by the Free Software Foundation; version 2
-   of the License.
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
 */
+
 #include "transferdetails.h"
 
 #include "ui_transferdetailsfrm.h"
