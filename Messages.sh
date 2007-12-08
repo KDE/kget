383439f351bb6806ac717ad3ff4585f096ac7b25
Thomas Reitelbach <tr@erdfunkstelle.de>
2007-12-08 15:55:25 +0000

    fix i18n extraction for KGet
    CCMAIL:kde-i18n-doc@kde.org
    
    svn path=/trunk/KDE/kdenetwork/kget/; revision=746292

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-$EXTRACTRC */*.ui >> rc.cpp || exit 11
-$EXTRACTRC *.rc */*.rc >> rc.cpp || exit 11
-$XGETTEXT *.cpp */*.cpp  -o $podir/kget.pot
+$EXTRACTRC `find . -name \*.ui` >> rc.cpp || exit 11
+$EXTRACTRC `find . -name \*.rc` >> rc.cpp || exit 11
+$XGETTEXT `find . -name \*.cpp \*.h`  -o $podir/kget.pot
 rm -f rc.cpp
