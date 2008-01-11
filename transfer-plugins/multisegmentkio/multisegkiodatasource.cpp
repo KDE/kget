9613247885ebb9c3d8268eaefcad87615bdbe43c
Manolo Valdes <nolis71cu@gmail.com>
2008-01-11 19:02:28 +0000

    oops i forgot to add this ones
    svn path=/trunk/KDE/kdenetwork/kget/; revision=760025

---
+++ transfer-plugins/multisegmentkio/multisegkiodatasource.cpp
@@ -0,0 +1,34 @@
+/* This file is part of the KDE project
+
+   Copyright (C) 2008 Manolo Valdes <nolis71cu@gmail.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+*/
+ 
+#include "multisegkiodatasource.h"
+#include <kdebug.h>
+
+MultiSegKioDataSource::MultiSegKioDataSource()
+:TransferDataSource(0)
+{
+   kDebug(5001);
+}
+
+void MultiSegKioDataSource::start()
+{
+   kDebug(5001);
+}
+
+void MultiSegKioDataSource::stop()
+{
+   kDebug(5001);
+}
+
+void MultiSegKioDataSource::addSegment(const KUrl &srcUrl, const KIO::fileoffset_t offset, const KIO::fileoffset_t bytes)
+{
+   kDebug(5001);
+}
+
---
+++ transfer-plugins/multisegmentkio/multisegkiodatasource.h
@@ -0,0 +1,28 @@
+/* This file is part of the KDE project
+
+   Copyright (C) 2008 Manolo Valdes <nolis71cu@gmail.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+*/
+ 
+#ifndef KGET_MULTISEGKIODATASOURCE_H
+#define KGET_MULTISEGKIODATASOURCE_H
+
+#include "core/transferdatasource.h"
+
+#include <kio/job.h>
+
+class MultiSegKioDataSource : public TransferDataSource
+{
+    public:
+        MultiSegKioDataSource();
+
+        void start();
+	void stop();
+        void addSegment(const KUrl &srcUrl, const KIO::fileoffset_t offset, const KIO::fileoffset_t bytes);
+};
+
+#endif
