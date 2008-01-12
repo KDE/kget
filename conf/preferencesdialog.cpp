290da5799bcd5d6a7e1c08b4c8d83bc7e0fac4b4
Laurent Montel <montel@kde.org>
2008-01-12 21:14:03 +0000

    Fix help button
    svn path=/trunk/KDE/kdenetwork/kget/; revision=760517

--- conf/preferencesdialog.cpp
+++ conf/preferencesdialog.cpp
@@ -51,6 +51,7 @@ PreferencesDialog::PreferencesDialog(QWidget * parent, KConfigSkeleton * skeleto
     addPage(advanced, i18nc("Advanced Options", "Advanced"), "preferences-other", i18n("Advanced Options"));
     addPage(plugins, i18n("Plugins"), "preferences-plugin", i18n("Transfer Plugin Options"));
 
+    setHelp(QString(),"kget");
     connect(this, SIGNAL(accepted()), SLOT(disableButtonApply()));
     connect(this, SIGNAL(rejected()), SLOT(disableButtonApply()));
 }
