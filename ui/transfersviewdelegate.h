cec495b9fae0a258cc796303a2a35f361b5f45bc
Urs Wolfer <uwolfer@kde.org>
2008-01-28 19:39:18 +0000

    SVN_SILENT Javier is right, Dario has also a copyright on these files. CCMAIL:jgoday@gmail.com
    svn path=/trunk/KDE/kdenetwork/kget/; revision=767783

--- ui/transfersviewdelegate.h
+++ ui/transfersviewdelegate.h
@@ -1,5 +1,6 @@
 /* This file is part of the KDE project
 
+   Copyright (C) 2006 Dario Massarin <nekkar@libero.it>
    Copyright (C) 2007 by Javier Goday <jgoday@gmail.com>
 
    This program is free software; you can redistribute it and/or
