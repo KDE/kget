7d31ef28694f02d79d4004619908b6ff5db3fbc5
Lukas Appelhans <l.appelhans@gmx.de>
2008-05-06 19:39:16 +0000

    Krazy fixes, especially i18n-ones
    svn path=/trunk/KDE/kdenetwork/kget/; revision=804695

--- transfer-plugins/bittorrent/advanceddetails/iwfiletreemodel.cpp
+++ transfer-plugins/bittorrent/advanceddetails/iwfiletreemodel.cpp
@@ -145,7 +145,7 @@ namespace kt
 				case 4: 
 				{
 					float percent = file->getDownloadPercentage();
-					return i18n("%1 %", KGlobal::locale()->formatNumber(percent, 2));;
+					return ki18n("%1 %").subs(percent, 0, 'g', 2).toString();
 				}
 				default: return QVariant();
 			}	
@@ -168,7 +168,7 @@ namespace kt
 				case 4: 
 				{
 					double percent = bt::Percentage(tc->getStats());
-					return i18n("%1 %", KGlobal::locale()->formatNumber(percent, 2));
+					return ki18n("%1 %").subs(percent, 0, 'g', 2).toString();
 				}
 				default: return QVariant();
 			}
