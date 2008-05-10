0c3d2fa59b73870d352e50264680673501ae05cc
Lukas Appelhans <l.appelhans@gmx.de>
2008-05-10 08:48:34 +0000

    Backport of rev.805445: Never use scientific notation when displaying percentages
    svn path=/trunk/KDE/kdenetwork/kget/; revision=806142

--- transfer-plugins/bittorrent/advanceddetails/iwfiletreemodel.cpp
+++ transfer-plugins/bittorrent/advanceddetails/iwfiletreemodel.cpp
@@ -145,7 +145,7 @@ namespace kt
 				case 4: 
 				{
 					float percent = file->getDownloadPercentage();
-					return ki18n("%1 %").subs(percent, 0, 'g', 2).toString();
+					return ki18n("%1 %").subs(percent, 0, 'f', 2).toString();
 				}
 				default: return QVariant();
 			}	
@@ -168,7 +168,7 @@ namespace kt
 				case 4: 
 				{
 					double percent = bt::Percentage(tc->getStats());
-					return ki18n("%1 %").subs(percent, 0, 'g', 2).toString();
+					return ki18n("%1 %").subs(percent, 0, 'f', 2).toString();
 				}
 				default: return QVariant();
 			}
