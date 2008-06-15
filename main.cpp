f9c29169fb8ef46be3b8ccd8ed780d8754483ddd
Lukas Appelhans <l.appelhans@gmx.de>
2008-06-15 13:52:34 +0000

    '' instead of "" (krazy fix)
    svn path=/trunk/KDE/kdenetwork/kget/; revision=820810

--- main.cpp
+++ main.cpp
@@ -111,7 +111,7 @@ private:
 int main(int argc, char *argv[])
 {
     KAboutData aboutData("kget", 0, ki18n("KGet"),
-                         QByteArray("2." + QByteArray::number(KDE_VERSION_MINOR) + "." + QByteArray::number(KDE_VERSION_RELEASE)),
+                         QByteArray("2." + QByteArray::number(KDE_VERSION_MINOR) + '.' + QByteArray::number(KDE_VERSION_RELEASE)),
                          ki18n("An advanced download manager for KDE"),
                          KAboutData::License_GPL,
                          ki18n("(C) 2005 - 2008, The KGet developers\n"
