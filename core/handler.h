09e22f1fbf8398037110f8daad99d0e3c079e564
Lukas Appelhans <l.appelhans@gmx.de>
2008-08-12 19:15:01 +0000

    Add email-tags n stuff
    svn path=/trunk/KDE/kdenetwork/kget/; revision=846004

--- core/handler.h
+++ core/handler.h
@@ -1,6 +1,6 @@
 /* This file is part of the KDE project
 
-   Copyright (C) 2008 Lukas Appelhans
+   Copyright (C) 2008 Lukas Appelhans <l.appelhans@gmx.de>
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public
