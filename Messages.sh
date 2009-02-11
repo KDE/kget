6b7ab878fd2e2d8359e001fe9f4dcad41d233d90
Laurent Montel <montel@kde.org>
2009-02-11 13:04:54 +0000

    Extract messages
    svn path=/branches/KDE/4.2/kdenetwork/kget/; revision=924704

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-kget_subdirs="conf core dbus transfer-plugins/kio transfer-plugins/metalink transfer-plugins/multisegmentkio transfer-plugins/bittorrent ui extensions/konqueror extensions/webinterface"
+kget_subdirs="conf core dbus transfer-plugins ui extensions/konqueror extensions/webinterface"
 $EXTRACTRC `find $kget_subdirs -name \*.ui` >> rc.cpp || exit 11
 $EXTRACTRC `find $kget_subdirs -name \*.rc` >> rc.cpp || exit 12
 $XGETTEXT `find $kget_subdirs -name \*.cpp -o -name \*.h` *.cpp *.h -o $podir/kget.pot
