502c442ff73884f22e0df62be25ad333d91ab075
Laurent Montel <montel@kde.org>
2009-03-25 12:17:28 +0000

    Extract messages from kcfg
    svn path=/trunk/KDE/kdenetwork/kget/; revision=944295

--- Messages.sh
+++ Messages.sh
@@ -2,5 +2,6 @@
 kget_subdirs="conf core dbus transfer-plugins ui extensions/konqueror extensions/webinterface"
 $EXTRACTRC `find $kget_subdirs -name \*.ui` >> rc.cpp || exit 11
 $EXTRACTRC `find $kget_subdirs -name \*.rc` >> rc.cpp || exit 12
+$EXTRACTRC `find $kget_subdirs -name \*.kcfg` >> rc.cpp || exit 13
 $XGETTEXT `find $kget_subdirs -name \*.cpp -o -name \*.h` *.cpp *.h -o $podir/kget.pot
 rm -f rc.cpp
