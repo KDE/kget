KDE/kget

commit 6f2834fa4cc3e9e569ccbcdcf876bdaad0f4a936

Removes the seperation of addSegment and addSegments, now one can only use addSegments. The TransferDataSources have to handle all that internally. Also removes the "offest" in addSegments, the offset has to be calculated by the TransferDataSources (segmentRange.first + segmentSize.first), the reason is to ensure that offset is always based on segmentRange and segmentSize and nothing else.
svn path=/trunk/KDE/kdenetwork/kget/; revision=1072308

core/datasourcefactory.cpp
@@ -722,14 +722,9 @@ void DataSourceFactory::assignSegments(TransferDataSource *source)
             return;
         }
 
-        if (source->canHandleMultipleSegments()) {
-            const QPair<int, int> splitResult = target->split();
-            newStart = splitResult.first;
-            newEnd = splitResult.second;
-        } else {
-            newStart = target->takeOneSegment();
-            newEnd = newStart;
-        }
+        const QPair<int, int> splitResult = target->split();
+        newStart = splitResult.first;
+        newEnd = splitResult.second;
     } else {
         m_startedChunks->getContinuousRange(&newStart, &newEnd, false);
     }
@@ -740,7 +735,6 @@ void DataSourceFactory::assignSegments(TransferDataSource *source)
         return;
     }
 
-    const KIO::fileoffset_t newoff = KIO::fileoffset_t(newStart * m_segSize);
     const KIO::fileoffset_t rest = m_size % m_segSize;
 
     //if newStart is the last segment of the download and there is a rest, when segSize is rest
@@ -749,12 +743,12 @@ void DataSourceFactory::assignSegments(TransferDataSource *source)
     //the lastSegsize is rest, but only if there is a rest and it is the last segment of the download
     const KIO::fileoffset_t lastSegSize = ((static_cast<uint>(newEnd + 1) == m_startedChunks->getNumBits() && rest) ? rest : m_segSize);
 
-    kDebug(5001) << "Segments assigned:" << newStart << "-" << newEnd << "offset:" << newoff << "segment-size:" << segSize << "rest:" << rest;
+    kDebug(5001) << "Segments assigned:" << newStart << "-" << newEnd << "segment-size:" << segSize << "rest:" << rest;
 
     for (int i = newStart; i <= newEnd; ++i) {
         m_startedChunks->set(i, true);
     }
-    source->addSegments(newoff, qMakePair(segSize, lastSegSize), qMakePair(newStart, newEnd));
+    source->addSegments(qMakePair(segSize, lastSegSize), qMakePair(newStart, newEnd));
 
     //there should still be segments added to this transfer
     if (source->changeNeeded() > 0) {

core/transferdatasource.h
@@ -40,34 +40,15 @@ class KGET_EXPORT TransferDataSource : public QObject
         virtual void stop() = 0;
 
         /**
-         * If true this TransferDataSource can handle multiple segments for one connection
-         * @note false by default
-         */
-        virtual bool canHandleMultipleSegments() const;
-
-        /**
-         * Add a segment to be downloaded by this TransferDataSource
-         * @param offset the offset of the file to be downloaded
-         * @param bytes number of bytes to be downloaded
-         * @param segmentNum the number of the segment to identify it
-         * @note instead of assigning only one segment (that results in one connection),
-         * one can assign multiple segments (all downloaded with one connection) via
-         * assignSegments, thus constant calling of addSegment and then reconnecting
-         * can be avoided
-         */
-        virtual void addSegment(const KIO::fileoffset_t offset, const KIO::fileoffset_t bytes, int segmentNum) = 0;//TODO
-
-        /**
          * Adds multiple continuous segments that should be downloaded by this TransferDataSource
-         * @param offset the offset of the file to be downloaded
          * @param segmentSize first is the general segmentSize, second the segmentSize
          * of the last segment in the range, if just one segment is assigned both need to have
-         * the same value
+         * the same value; segmentSize.first can be used to calculate the offset
          * @param segmentRange first the beginning, second the end
          * @note the default implemention will just call addSegment multiple times, then
          * split -- even if implemented -- would not work
          */
-        virtual void addSegments(const KIO::fileoffset_t offset, const QPair<KIO::fileoffset_t, KIO::fileoffset_t> &segmentSize, const QPair<int, int> &segmentRange);
+        virtual void addSegments(const QPair<KIO::fileoffset_t, KIO::fileoffset_t> &segmentSize, const QPair<int, int> &segmentRange) = 0;
 
         /**
          * Removes one connection, useful when setMaximumParalellDownloads was called with a lower number

transfer-plugins/multisegmentkio/segment.cpp
@@ -18,10 +18,10 @@
 
 #include <QtCore/QTimer>
 
-Segment::Segment(const KUrl &src, const KIO::fileoffset_t offset, const QPair<KIO::fileoffset_t, KIO::fileoffset_t> &segmentSize, const QPair<int, int> &segmentRange, QObject *parent)
+Segment::Segment(const KUrl &src, const QPair<KIO::fileoffset_t, KIO::fileoffset_t> &segmentSize, const QPair<int, int> &segmentRange, QObject *parent)
   : QObject(parent),
     m_status(Stopped),
-    m_offset(offset),
+    m_offset(segmentSize.first * segmentRange.first),
     m_segSize(segmentSize),
     m_currentSegSize(m_segSize.first),
     m_currentSegment(segmentRange.first),

core/transferdatasource.cpp
@@ -28,27 +28,6 @@ TransferDataSource::~TransferDataSource()
     kDebug(5001) ;
 }
 
-bool TransferDataSource::canHandleMultipleSegments() const
-{
-    return false;
-}
-
-void TransferDataSource::addSegments(const KIO::fileoffset_t offset, const QPair<KIO::fileoffset_t, KIO::fileoffset_t> &segmentSize, const QPair<int, int> &segmentRange)
-{
-    int k = 0;
-
-    for (int i = segmentRange.first; i <= segmentRange.second; ++i)
-    {
-        KIO::fileoffset_t bytes = segmentSize.first;
-        if (i == segmentRange.second)
-        {
-            bytes = segmentSize.second;
-        }
-        addSegment(offset + k * segmentSize.first, bytes, i);
-        ++k;
-    }
-}
-
 QPair<int, int> TransferDataSource::removeConnection()
 {
     return QPair<int, int>(-1, -1);

transfer-plugins/bittorrent/btdatasource.h
@@ -36,7 +36,7 @@ class BTDataSource : public TransferDataSource
 
         void start();
         void stop();
-        void addSegment(const KIO::fileoffset_t offset, const KIO::fileoffset_t bytes, int segmentNum);
+        void addSegments(const QPair<KIO::fileoffset_t, KIO::fileoffset_t> &segmentSize, const QPair<int, int> &segmentRange);
         void getData(const KIO::fileoffset_t &off, const QByteArray &dataArray);
 
     private slots:

transfer-plugins/checksumsearch/checksumsearchtransferdatasource.cpp
@@ -59,12 +59,10 @@ void ChecksumSearchTransferDataSource::stop()
 {
     kDebug(5001);
 }
-
-void ChecksumSearchTransferDataSource::addSegment(const KIO::fileoffset_t offset, const KIO::fileoffset_t bytes, int segmentNum)
+void ChecksumSearchTransferDataSource::addSegments(const QPair<KIO::fileoffset_t, KIO::fileoffset_t> &segmentSize, const QPair<int, int> &segmentRange)
 {
-    Q_UNUSED(offset)
-    Q_UNUSED(bytes)
-    Q_UNUSED(segmentNum)
+    Q_UNUSED(segmentSize)
+    Q_UNUSED(segmentRange)
     kDebug(5001);
 }
 

transfer-plugins/checksumsearch/checksumsearchtransferdatasource.h
@@ -35,7 +35,7 @@ class ChecksumSearchTransferDataSource : public TransferDataSource
 
         void start();
         void stop();
-        void addSegment(const KIO::fileoffset_t offset, const KIO::fileoffset_t bytes, int segmentNum = -1);
+        void addSegments(const QPair<KIO::fileoffset_t, KIO::fileoffset_t> &segmentSize, const QPair<int, int> &segmentRange);
 
     private:
         KUrl m_src;

transfer-plugins/mirrorsearch/mirrorsearchtransferdatasource.cpp
@@ -31,11 +31,10 @@ void MirrorSearchTransferDataSource::stop()
     kDebug(5001);
 }
 
-void MirrorSearchTransferDataSource::addSegment(const KIO::fileoffset_t offset, const KIO::fileoffset_t bytes, int segmentNum)
+void MirrorSearchTransferDataSource::addSegments(const QPair<KIO::fileoffset_t, KIO::fileoffset_t> &segmentSize, const QPair<int, int> &segmentRange)
 {
-    Q_UNUSED(offset)
-    Q_UNUSED(bytes)
-    Q_UNUSED(segmentNum)
+    Q_UNUSED(segmentSize)
+    Q_UNUSED(segmentRange)
     kDebug(5001);
 }
 

commit d5eea069d258568482e6129dd93d850a4790b741

Fix the calculation of offset if it is the last segment, bug introduced with r1072308
svn path=/trunk/KDE/kdenetwork/kget/; revision=1080197

core/datasourcefactory.cpp
@@ -737,18 +737,15 @@ void DataSourceFactory::assignSegments(TransferDataSource *source)
 
     const KIO::fileoffset_t rest = m_size % m_segSize;
 
-    //if newStart is the last segment of the download and there is a rest, when segSize is rest
-    const KIO::fileoffset_t segSize = ((static_cast<uint>(newStart + 1) == m_startedChunks->getNumBits()) && rest) ? rest : m_segSize;
-
     //the lastSegsize is rest, but only if there is a rest and it is the last segment of the download
     const KIO::fileoffset_t lastSegSize = ((static_cast<uint>(newEnd + 1) == m_startedChunks->getNumBits() && rest) ? rest : m_segSize);
 
-    kDebug(5001) << "Segments assigned:" << newStart << "-" << newEnd << "segment-size:" << segSize << "rest:" << rest;
+    kDebug(5001) << "Segments assigned:" << newStart << "-" << newEnd << "segment-size:" << m_segSize << "rest:" << rest;
 
     for (int i = newStart; i <= newEnd; ++i) {
         m_startedChunks->set(i, true);
     }
-    source->addSegments(qMakePair(segSize, lastSegSize), qMakePair(newStart, newEnd));
+    source->addSegments(qMakePair(m_segSize, lastSegSize), qMakePair(newStart, newEnd));
 
     //there should still be segments added to this transfer
     if (source->changeNeeded() > 0) {

core/transferdatasource.h
@@ -41,12 +41,10 @@ class KGET_EXPORT TransferDataSource : public QObject
 
         /**
          * Adds multiple continuous segments that should be downloaded by this TransferDataSource
-         * @param segmentSize first is the general segmentSize, second the segmentSize
-         * of the last segment in the range, if just one segment is assigned both need to have
-         * the same value; segmentSize.first can be used to calculate the offset
+         * @param segmentSize first is always the general segmentSize, second the segmentSize
+         * of the last segment in the range. If just one (the last) segment was assigned, then
+         * first would not equal second, this is to ensure that first can be used to calculate the offset
          * @param segmentRange first the beginning, second the end
-         * @note the default implemention will just call addSegment multiple times, then
-         * split -- even if implemented -- would not work
          */
         virtual void addSegments(const QPair<KIO::fileoffset_t, KIO::fileoffset_t> &segmentSize, const QPair<int, int> &segmentRange) = 0;
 

transfer-plugins/multisegmentkio/segment.cpp
@@ -32,6 +32,10 @@ Segment::Segment(const KUrl &src, const QPair<KIO::fileoffset_t, KIO::fileoffset
     m_canResume(true),
     m_url(src)
 {
+    //last segment
+    if (m_endSegment - m_currentSegment == 0) {
+        m_currentSegSize = m_segSize.second;
+    }
 }
 
 Segment::~Segment()