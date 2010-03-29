d53daf6b2d9378841f77fb34b296e183668530fa
Dawit Alemayehu <adawit@kde.org>
2010-03-29 13:52:43 +0000

    Added copyright.
    svn path=/branches/KDE/4.4/kdenetwork/kget/; revision=1108639

--- extensions/konqueror/kget_plug_in.cpp
+++ extensions/konqueror/kget_plug_in.cpp
@@ -3,6 +3,7 @@
    Copyright (C) 2002 Patrick Charbonnier <pch@valleeurpe.net>
    Copyright (C) 2002 Carsten Pfeiffer <pfeiffer@kde.org>
    Copyright (C) 2007 Urs Wolfer <uwolfer @ kde.org>
+   Copyright (C) 2010 Dawit Alemayehu <adawit@kde.org>
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public
