e67c3b36d883d3224ca1e60657bc1f79606e5ad1
Matthias Fuchs <mat69@gmx.net>
2011-02-04 17:33:24 +0000

    Backport r1218792 Extracts messages for konqueror extension to make it translatable. CCBUG:265140
    svn path=/branches/KDE/4.6/kdenetwork/kget/; revision=1218793

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-kget_subdirs="conf core dbus transfer-plugins ui extensions/konqueror extensions/webinterface"
+kget_subdirs="conf core dbus transfer-plugins ui extensions/webinterface"
 $EXTRACTRC `find $kget_subdirs -name \*.ui` >> rc.cpp || exit 11
 $EXTRACTRC `find $kget_subdirs -name \*.rc` >> rc.cpp || exit 12
 $EXTRACTRC `find $kget_subdirs -name \*.kcfg` >> rc.cpp || exit 13
