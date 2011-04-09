90b9675e4f0db9cb456a1b6e776b9a54fa6f9ce8
Matthias Fuchs <mat69@gmx.net>
2011-04-09 09:26:37 +0000

    Further addresses CVE-2010-1000. The file name of Metalink File is checked a better way, making it work under more conditions.
    svn path=/branches/KDE/4.6/kdenetwork/kget/; revision=1227471

--- ui/metalinkcreator/metalinker.cpp
+++ ui/metalinkcreator/metalinker.cpp
@@ -583,7 +583,13 @@ bool KGetMetalink::File::isValidNameAttribute() const
         return false;
     }
 
-    if (name.contains(QRegExp("$(\\.\\.?)?/")) || name.contains("/../") || name.endsWith("/..")) {
+    if (name.endsWith('/')) {
+        kError(5001) << "Name attribute of Metalink::File does not contain a file name:" << name;
+        return false;
+    }
+
+    const QStringList components = name.split('/');
+    if (name.startsWith('/') || components.contains("..") || components.contains(".")) {
         kError(5001) << "Name attribute of Metalink::File contains directory traversal directives:" << name;
         return false;
     }
