497fbc5f3ebbebe0d1f84219e588432c4ea11bc3
Raphael Kubo da Costa <rakuco@FreeBSD.org>
2013-05-20 13:16:41 +0000

    Fix most-vexing parse error introduced in r1345874.
    By the way, why is this revision only in branches/KDE/4.10 and not in trunk?
    
    CCMAIL: david.narvaez@computer.org
    
    svn path=/branches/KDE/4.10/kdenetwork/kget/; revision=1355676

--- core/transferhistorystore.cpp
+++ core/transferhistorystore.cpp
@@ -554,7 +554,7 @@ NepomukStore::~NepomukStore()
 void NepomukStore::load()
 {
     Nepomuk::Query::QueryServiceClient * queryService = new Nepomuk::Query::QueryServiceClient(this);
-    Nepomuk::Query::ResourceTypeTerm historyTypeTerm(Nepomuk::Types::Class(Nepomuk::HistoryItem::resourceTypeUri()));
+    Nepomuk::Query::ResourceTypeTerm historyTypeTerm((Nepomuk::Types::Class(Nepomuk::HistoryItem::resourceTypeUri())));
     Nepomuk::Query::Query historyQuery(historyTypeTerm);
 
     connect(queryService, SIGNAL(newEntries(const QList<Nepomuk::Query::Result>&)), this, SLOT(loadResult(const QList<Nepomuk::Query::Result>&)));

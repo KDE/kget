KDE/kget

commit 4e60480f9014ffd6f8d2a42679994a5d3b601416

Improving Nepomuk code efficiency by using the Query API and clearing HistoryItems using the list already loaded (plus some checks to work around Nepomuk bugs)
svn path=/branches/KDE/4.10/kdenetwork/kget/; revision=1345874

core/transferhistorystore.cpp
@@ -33,6 +33,11 @@
     #include "historyitem.h"
 
     #include <Soprano/Vocabulary/RDF>
+    #include <Nepomuk/Query/Query>
+    #include <Nepomuk/Query/QueryServiceClient>
+    #include <Nepomuk/Query/ResourceTypeTerm>
+    #include <Nepomuk/Query/Result>
+    #include <Nepomuk/Types/Class>
     #include <Nepomuk/Variant>
 #endif
 
@@ -548,10 +553,27 @@ NepomukStore::~NepomukStore()
 
 void NepomukStore::load()
 {
-    QList<Nepomuk::HistoryItem> allItems = Nepomuk::HistoryItem::allHistoryItems();
+    Nepomuk::Query::QueryServiceClient * queryService = new Nepomuk::Query::QueryServiceClient(this);
+    Nepomuk::Query::ResourceTypeTerm historyTypeTerm(Nepomuk::Types::Class(Nepomuk::HistoryItem::resourceTypeUri()));
+    Nepomuk::Query::Query historyQuery(historyTypeTerm);
 
-    for (int i = 0; i != allItems.count(); i++) {
-        Nepomuk::HistoryItem current = allItems.at(i);
+    connect(queryService, SIGNAL(newEntries(const QList<Nepomuk::Query::Result>&)), this, SLOT(loadResult(const QList<Nepomuk::Query::Result>&)));
+    connect(queryService, SIGNAL(finishedListing()), this, SIGNAL(loadFinished()));
+    queryService->query(historyQuery);
+}
+
+void NepomukStore::loadResult(const QList<Nepomuk::Query::Result>& entries)
+{
+    int newItemsCount = m_items.count() + entries.count();
+
+    for (int i = 0; i != entries.count(); i++) {
+        Nepomuk::HistoryItem current = entries.at(i).resource();
+
+        if (!current.dateTime().isValid()) {
+            newItemsCount--;
+            continue;
+        }
+
         TransferHistoryItem item;
         item.setDest(current.destination());
         item.setSource(current.source());
@@ -559,17 +581,17 @@ void NepomukStore::load()
         item.setDateTime(current.dateTime());
         item.setSize(current.size());
         m_items << item;
-        emit elementLoaded(i + 1, allItems.count(), item);
+        emit elementLoaded(m_items.count(), newItemsCount, item);
     }
-
-    emit loadFinished();
 }
 
 void NepomukStore::clear()
 {
-    foreach (Nepomuk::HistoryItem item, Nepomuk::HistoryItem::allHistoryItems()) {
-        item.remove();
+    for(int i = 0; i < m_items.count(); i++) {
+        deleteItem(m_items.at(i));
     }
+
+    m_items.clear();
 }
 
 void NepomukStore::saveItem(const TransferHistoryItem &item)
@@ -589,11 +611,6 @@ void NepomukStore::deleteItem(const TransferHistoryItem &item)
 {
     Nepomuk::HistoryItem historyItem(item.source());
     historyItem.remove();
-    /*foreach (Nepomuk::HistoryItem it, Nepomuk::HistoryItem::allHistoryItems()) {
-        if (it.source() == item.source()) {
-            it.remove();
-        }
-    }    */
 }
 #endif
 

CMakeLists.txt
@@ -154,7 +154,7 @@ if (KDE4WORKSPACE_FOUND)
 endif (KDE4WORKSPACE_FOUND)
 
 if (HAVE_NEPOMUK)
-  target_link_libraries(kgetcore ${SOPRANO_LIBRARIES} ${NEPOMUK_LIBRARIES})
+  target_link_libraries(kgetcore ${SOPRANO_LIBRARIES} ${NEPOMUK_LIBRARIES} ${NEPOMUK_QUERY_LIBRARIES})
 endif (HAVE_NEPOMUK)
 
 if (SQLITE_FOUND)

core/transferhistorystore_nepomuk_p.h
@@ -10,7 +10,18 @@
 #ifndef TRANSFERHISTORYSTORE_NEPOMUK_P_H
 #define TRANSFERHISTORYSTORE_NEPOMUK_P_H
 
+#include "transferhistorystore.h"
+
 #include <QList>
+
+namespace Nepomuk
+{
+    namespace Query
+    {
+        class Result;
+        class QueryServiceClient;
+    }
+}
 
 class TransferHistoryItem;
 class NepomukStore : public TransferHistoryStore
@@ -22,6 +33,7 @@ public:
 
 public slots:
     void load();
+    void loadResult(const QList<Nepomuk::Query::Result>& entries);
     void clear();
     void saveItem(const TransferHistoryItem &item);
     void deleteItem(const TransferHistoryItem &item);

commit 497fbc5f3ebbebe0d1f84219e588432c4ea11bc3

Fix most-vexing parse error introduced in r1345874.
By the way, why is this revision only in branches/KDE/4.10 and not in trunk?

CCMAIL: david.narvaez@computer.org

svn path=/branches/KDE/4.10/kdenetwork/kget/; revision=1355676

core/transferhistorystore.cpp
@@ -554,7 +554,7 @@ NepomukStore::~NepomukStore()
 void NepomukStore::load()
 {
     Nepomuk::Query::QueryServiceClient * queryService = new Nepomuk::Query::QueryServiceClient(this);
-    Nepomuk::Query::ResourceTypeTerm historyTypeTerm(Nepomuk::Types::Class(Nepomuk::HistoryItem::resourceTypeUri()));
+    Nepomuk::Query::ResourceTypeTerm historyTypeTerm((Nepomuk::Types::Class(Nepomuk::HistoryItem::resourceTypeUri())));
     Nepomuk::Query::Query historyQuery(historyTypeTerm);
 
     connect(queryService, SIGNAL(newEntries(const QList<Nepomuk::Query::Result>&)), this, SLOT(loadResult(const QList<Nepomuk::Query::Result>&)));