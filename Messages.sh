172877bfc61c8ac298d244b68c347c723a4cc6b3
Albert Astals Cid <aacid@kde.org>
2021-12-15 19:46:31 +0100

    extension is gone
    CCMAIL: alexander.lohnau@gmx.de

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-kget_subdirs="conf core dbus transfer-plugins ui extensions/webinterface"
+kget_subdirs="conf core dbus transfer-plugins ui"
 $EXTRACTRC `find $kget_subdirs -name \*.ui` >> rc.cpp || exit 11
 $EXTRACTRC `find $kget_subdirs -name \*.rc` >> rc.cpp || exit 12
 $EXTRACTRC `find $kget_subdirs -name \*.kcfg` >> rc.cpp || exit 13
